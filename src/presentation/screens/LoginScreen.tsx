6cc906aa5461afad8100a892f0b4355c04fd33be
Daniel <rayodani93gmail.com>
2024-09-12 14:55:50 +0200

    Añadido el TopBar y ajustes en las pantallas de Goleadores y Registro

--- src/presentation/screens/LoginScreen.tsx
+++ src/presentation/screens/LoginScreen.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ImageBackground } from 'react-native';
 import { TextInput, Button, Title } from 'react-native-paper';
 import { LoginScreenNavigationProp } from '../../types/navigation';
-import { colors } from '../../types/theme'; // Importamos los colores
+import { colors } from '../../types/theme';
 
 type Props = {
   navigation: LoginScreenNavigationProp;
@@ -13,93 +13,100 @@ const LoginScreen: React.FC<Props> = ({ navigation }) => {
   const [password, setPassword] = useState('');
 
   return (
-    <View style={styles.container}>
-      <View style={styles.formContainer}>
-        <Title style={styles.title}>Iniciar Sesión</Title>
+    <ImageBackground
+      source={require('../../assets/FondoInicioSesion.jpg')}
+      style={styles.backgroundImage}
+    >
+      <View style={styles.container}>
+        <View style={styles.formContainer}>
+          <Title style={styles.title}>Iniciar Sesión</Title>
 
-        <TextInput
-          label="Correo Electrónico"
-          value={email}
-          mode="outlined"
-          onChangeText={setEmail}
-          style={styles.input}
-          keyboardType="email-address"
-          autoCapitalize="none"
-          autoComplete="email"
-          theme={{ colors: { text: colors.primary, primary: colors.primary } }} // Texto y bordes en azul
-        />
+          <TextInput
+            label="Correo Electrónico"
+            value={email}
+            mode="outlined"
+            onChangeText={setEmail}
+            style={styles.input}
+            keyboardType="email-address"
+            autoCapitalize="none"
+            autoComplete="email"
+            theme={{ colors: { text: colors.primary, primary: colors.primary } }} // Texto y bordes en azul
+          />
 
-        <TextInput
-          label="Contraseña"
-          value={password}
-          mode="outlined"
-          onChangeText={setPassword}
-          style={styles.input}
-          secureTextEntry
-          theme={{ colors: { text: colors.primary, primary: colors.primary } }} // Texto y bordes en azul
-        />
+          <TextInput
+            label="Contraseña"
+            value={password}
+            mode="outlined"
+            onChangeText={setPassword}
+            style={styles.input}
+            secureTextEntry
+            theme={{ colors: { text: colors.primary, primary: colors.primary } }} // Texto y bordes en azul
+          />
 
-        {/* Botón de inicio de sesión con fondo blanco y texto en azul más fuerte */}
-        <Button
-          mode="contained"
-          onPress={() => console.log('Iniciar sesión')}
-          style={styles.loginButton}
-          labelStyle={{ color: colors.strongBlue }} // Texto en azul más fuerte
-        >
-          Iniciar Sesión
-        </Button>
+          <Button
+            mode="contained"
+            onPress={() => console.log('Iniciar sesión')}
+            style={styles.loginButton}
+            labelStyle={{ color: colors.strongBlue }} // Texto en azul más fuerte
+          >
+            Iniciar Sesión
+          </Button>
 
-        {/* Botón de registro con texto en azul más fuerte */}
-        <Button
-          mode="text"
-          onPress={() => navigation.navigate('Register')}
-          labelStyle={styles.registerButtonText} // Estilo del texto directamente
-          style={styles.registerButton}
-        >
-          ¿No tienes cuenta? Regístrate
-        </Button>
+          <Button
+            mode="text"
+            onPress={() => navigation.navigate('Register')}
+            labelStyle={styles.registerButtonText} // Estilo del texto directamente
+            style={styles.registerButton}
+          >
+            ¿No tienes cuenta? Regístrate
+          </Button>
+        </View>
       </View>
-    </View>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover', // Ajusta la imagen al tamaño de la pantalla
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: colors.white, // Fondo blanco
+    padding: 20,
   },
   formContainer: {
     width: '90%',
+    backgroundColor: 'rgba(255, 255, 255, 0.9)', // Fondo blanco semitransparente
     padding: 20,
-    backgroundColor: 'transparent', // Elimina cualquier cuadro de fondo innecesario
+    borderRadius: 10,
   },
   title: {
     fontSize: 24,
     marginBottom: 20,
     fontWeight: 'bold',
     textAlign: 'center',
-    color: colors.strongBlue, // Texto en azul
+    color: colors.strongBlue,
   },
   input: {
     marginBottom: 20,
-    backgroundColor: colors.white, // Fondo blanco para el input
-    borderColor: colors.primary, // Borde azul para los inputs
+    backgroundColor: colors.white,
   },
   loginButton: {
     marginTop: 10,
-    borderRadius: 30, // Borde redondeado
-    backgroundColor: colors.white, // Fondo blanco
-    borderColor: colors.strongBlue, // Borde azul fuerte
-    borderWidth: 2, // Ancho del borde
+    borderRadius: 30,
+    backgroundColor: colors.white,
+    borderColor: colors.strongBlue,
+    borderWidth: 2,
   },
   registerButton: {
     marginTop: 10,
     alignItems: 'center',
   },
   registerButtonText: {
-    color: colors.strongBlue, // Texto en azul más fuerte
+    color: colors.strongBlue,
     fontWeight: 'bold',
     fontSize: 16,
     textTransform: 'uppercase',
